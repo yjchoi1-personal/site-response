4b02ff1a073edb3b4235d563f52ccc9ff097b512
baagee <yjchoi9212@gmail.com>
2024-04-30 09:54:17 -0500

    update loss plot tool

--- tools/plot_loss.py
+++ tools/plot_loss.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import pickle
 
-with open('loss_histories.pkl', 'rb') as f:
+site_id = 'FKSH17-transformer_standardization'
+checkpoint_path = f'../data/checkpoints/{site_id}/'
+with open(f'{checkpoint_path}/loss_histories.pkl', 'rb') as f:
     loss_data = pickle.load(f)
     train_losses = loss_data['train_losses']
     valid_losses = loss_data['valid_losses']
@@ -14,6 +16,8 @@ if valid_losses:
     plt.plot([loss[0] for loss in valid_losses], [loss[1] for loss in valid_losses], label='Validation Loss')
 plt.xlabel('Iteration')
 plt.ylabel('Loss')
+plt.yscale('log')
 plt.title('Training and Validation Loss Over Time')
 plt.legend()
+plt.savefig(f"{checkpoint_path}/loss_hist.png")
 plt.show()
